1be475ed0b7e1f97c5274d848d0049c17418658f
konstantinos katrioplas <kokatrio@gmail.com>
2018-12-28 16:53:28 +0200

    retrieving page from last tweet

--- post_update.py
+++ post_update.py
@@ -1,8 +1,9 @@
 import twitter
 import json
 import scrap_wikipedia
+import wikipediaapi
 
-'''
+
 filename = 'cred.json'
 with open(filename) as f:
     data = json.load(f)
@@ -11,7 +12,7 @@ api = twitter.Api(consumer_key=data['consumer key'],
                   consumer_secret=data['consumer secret'],
                   access_token_key=data['access token key'],
                   access_token_secret=data['access token secret'])
-'''
+
 
 def post_tweet():
 
@@ -26,11 +27,22 @@ def post_tweet():
         sys.exit(2)
         print("{0} just posted: {1}".format(status.user.name, status.text))
 
-
 def process_tweet(content):
     print(content)
 
+def get_last_link():
+    statuses = api.GetUserTimeline(screen_name='bitonic5000')
+    return scrap_wikipedia.retrieve_last_node(statuses)
 
-content = scrap_wikipedia.wiki_content()
+
+# get last tweet
+last_page = get_last_link()
+print(last_page)
+
+# scrap article from last link
+try:
+    content = scrap_wikipedia.wiki_content(last_page)
+except AssertionError:
+    print("Can't scrap wiki page")
 
 process_tweet(content)
--- scrap_wikipedia.py
+++ scrap_wikipedia.py
@@ -1,12 +1,14 @@
 import wikipediaapi
 import random
 
+# must be taken from file
+#last_link = 'Python_(programming_language)'
 
-def wiki_content():
+def wiki_content(link):
 
     # starting page
     wiki_wiki = wikipediaapi.Wikipedia('en')
-    page = wiki_wiki.page('Python_(programming_language)')
+    page = wiki_wiki.page(link)
     print("Page - Exists: %s" % page.exists())
 
     # list with links
@@ -23,8 +25,18 @@ def wiki_content():
 
     # get next page
     next_page = wiki_wiki.page(next_link_title)
-    print("Next Page - Exists: %s" % page.exists())
+    print("Next Page - Exists: %s" % next_page.exists())
 
     content = next_link_title + ' - ' + next_page.fullurl
-
     return content
+
+
+def retrieve_last_node(statuses):
+    last_status = statuses[0]
+    last_text = last_status.text
+
+    wiki_wiki = wikipediaapi.Wikipedia('en')
+    last_page = wiki_wiki.page(last_text)
+    print("Last Page - Exists: %s" % last_page.exists())
+
+    return last_page
